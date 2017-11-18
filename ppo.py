64966a1f7d2292146950fad828b287b5d2e455a3
Hwhitetooth <hwhitetooth@gmail.com>
2017-11-18 01:06:08 -0500

    Corrected curriculum strategy.

--- ppo.py
+++ ppo.py
@@ -9,57 +9,46 @@ import os
 import time
 from env_wrapper import Env
 
-def rollout(env, pi, horizon, episodes, render = False):
-    s = env.reset()
-    s_batch = np.array([s for _ in range(horizon)])
-    a_batch = np.array([env.action_space.sample() for _ in range(horizon)])
-    r_batch = np.zeros(horizon, "float32")
-    v_batch = np.zeros(horizon, "float32")
-    done_batch = np.array([False] * horizon)
+def rollout(env, pi, goals, episodes_per_goal = 1, render = False):
+    s_batch, a_batch, r_batch, v_batch, done_batch = [], [], [], [], []
     ep_length = []
     ep_reward = []
-    cur_ep_length = cur_ep_reward = t = 0
-    while True:
-        a, v = pi.get_a_and_v(s[None])
-        # FIXME: a bug here
-        if np.any(np.isnan(a)):
-            a = env.action_space.sample()
-        s_next, r, done, _ = env.step(np.array(a))
-        if render:
-            env.render()
-        s_batch[t] = s
-        a_batch[t] = a
-        r_batch[t] = r
-        v_batch[t] = v
-        done_batch[t] = done
-        t += 1
-        cur_ep_length += 1
-        cur_ep_reward += r
-        s = s_next
-        if done:
-            ep_length.append(cur_ep_length)
-            ep_reward.append(cur_ep_reward)
-            episodes -= 1
-            if episodes == 0:
-                v_next = 0
-                yield {"s": s_batch, "a": a_batch, "r": r_batch, "v": v_batch, "done": done_batch, "v_next": v_next,
-                        "ep_length": ep_length, "ep_reward": ep_reward}
-                return
+    for gx, gy in goals:
+        env.reset(gx, gy)
+        for ep in range(episodes_per_goal):
             cur_ep_length = cur_ep_reward = 0
             s = env.reset()
-        if t == horizon:
-            v_next = 0 if done else pi.get_v(s[None])
-            yield {"s": s_batch, "a": a_batch, "r": r_batch, "v": v_batch, "done": done_batch, "v_next": v_next,
-                    "ep_length": ep_length, "ep_reward": ep_reward}
-            t = 0
-            ep_length = []
-            ep_reward = []
+            done = False
+            while not done:
+                a, v = pi.get_a_and_v(s[None])
+                # FIXME: a bug here
+                if np.any(np.isnan(a)):
+                    a = env.action_space.sample()
+                s_next, r, done, _ = env.step(np.array(a))
+                if render:
+                    env.render()
+                s_batch.append(s)
+                a_batch.append(a)
+                r_batch.append(r)
+                v_batch.append(v)
+                done_batch.append(done)
+                cur_ep_length += 1
+                cur_ep_reward += r
+                s = s_next
+            ep_length.append(cur_ep_length)
+            ep_reward.append(cur_ep_reward)
+    return {"s": np.array(s_batch),
+            "a": np.array(a_batch),
+            "r": np.array(r_batch),
+            "v": np.array(v_batch),
+            "done": np.array(done_batch)}, ep_reward
 
-def process_data(raw, horizon, gamma, lamda):
-    s_batch, a_batch, r_batch, v_batch, done_batch, v_next  = raw["s"], raw["a"], raw["r"], raw["v"], raw["done"], raw["v_next"]
+def process_data(raw, gamma, lamda):
+    s_batch, a_batch, r_batch, v_batch, done_batch = raw["s"], raw["a"], raw["r"], raw["v"], raw["done"]
+    horizon = s_batch.shape[0]
     g_batch = np.zeros(horizon, "float32")
     adv_batch = np.zeros(horizon, "float32")
-    adv_next = 0
+    v_next = adv_next = 0
     for i in reversed(range(horizon)):
         done = done_batch[i]
         r = r_batch[i]
@@ -76,7 +65,6 @@ def process_data(raw, horizon, gamma, lamda):
     data.pop("r")
     data.pop("v")
     data.pop("done")
-    data.pop("v_next")
     data["g"] = g_batch
     data["adv"] = adv_batch
     return data
@@ -84,8 +72,8 @@ def process_data(raw, horizon, gamma, lamda):
 def train(env_name,
         sess,
         policy_fn,
-        gamma = 0.99,
-        lamda = 0.95,
+        gamma = 0.998,
+        lamda = 0.995,
         horizon = 2048,
         batch_size = 64,
         epochs = 10,
@@ -123,108 +111,71 @@ def train(env_name,
     lr = tf.train.polynomial_decay(lr, step, max_steps, 0.0)
     opt = tf.train.AdamOptimizer(lr, epsilon = 1E-5)
     train_op = opt.minimize(total_loss, var_list = pi.vars)
-    saver = tf.train.Saver(max_to_keep = max_steps // (horizon * save_freqency) + 1)
-
-    # Summary.
-    summary_reward = tf.placeholder(tf.float32)
-    tf.summary.scalar("Average episode reward", summary_reward)
-    summary_length = tf.placeholder(tf.float32)
-    tf.summary.scalar("Average episode length", summary_length)
-    '''
-    summary_losses = dict()
-    for name, loss in zip(losses_names, losses):
-        summary_losses[name] = tf.placeholder(tf.float32)
-        tf.summary.scalar("loss_" + name, summary_losses[name])
-    tf.summary.scalar("Learning rate", lr)
-    summary_op = tf.summary.merge_all()
-    summary_writer = tf.summary.FileWriter(logdir, graph = sess.graph)
-    '''
 
     # Training.
     sess.run(tf.global_variables_initializer())
 
-    def update_policy(env, targets_buffer, episodes = 10):
-        targets = np.random.choice(len(targets_buffer), 10)
-        ep = 0
-        for i in targets:
-            tx, ty = targets_buffer[i]
-            env.reset(tx, ty)
-            data_gen = rollout(env, pi, horizon, episodes, render)
-            for raw_data in data_gen:
-                raw_data.pop("ep_length")
-                raw_data.pop("ep_reward")
-                data = Dataset(process_data(raw_data, horizon, gamma, lamda), batch_size)
-                sess.run(update_old)
-                for _ in range(epochs):
-                    for batch in data:
-                        train_feed= {pi.inputs: batch["s"], pi_old.inputs: batch["s"], a_taken: batch["a"], \
-                                adv_sample: batch["adv"], v_sample: batch["g"], step: it * horizon}
-                        sess.run(train_op, train_feed)
-            ep += 1
-            print(ep)
-        '''
-        stats_length = deque(maxlen = 100)
-        stats_reward = deque(maxlen = 100)
-        stats_length.extend(raw_data.pop("ep_length"))
-        stats_reward.extend(raw_data.pop("ep_reward"))
-        '''
+    def update_policy(env, goals, iterations = 5):
+        for it in range(iterations):
+            raw_data, _ = rollout(env, pi, goals, 1, render)
+            data = Dataset(process_data(raw_data, gamma, lamda), batch_size)
+            sess.run(update_old)
+            for _ in range(epochs):
+                for batch in data:
+                    train_feed= {pi.inputs: batch["s"], pi_old.inputs: batch["s"], a_taken: batch["a"], \
+                            adv_sample: batch["adv"], v_sample: batch["g"], step: it * horizon}
+                    sess.run(train_op, train_feed)
 
-    def eval_policy(env, targets):
-        stats_length = deque(maxlen = len(targets))
-        stats_reward = deque(maxlen = len(targets))
-        for tx, ty in targets:
-            env.reset(tx, ty)
-            data_gen = rollout(env, pi, horizon, 1, render)
-            for raw_data in data_gen:
-                stats_length.extend(raw_data.pop("ep_length"))
-                stats_reward.extend(raw_data.pop("ep_reward"))
-        mean_length = np.mean(np.array(stats_length), axis = 0)
-        mean_reward = np.mean(np.array(stats_reward), axis = 0)
-        return mean_length, mean_reward
+    def eval_policy(env, goals):
+        results = []
+        for gx, gy in goals:
+            _, ep_reward = rollout(env, pi, [(gx, gy)], 10)
+            score = sum(ep_reward) / len(ep_reward)
+            results.append((score, (gx, gy)))
+        return results
 
-    radius = 0.1
+    score_min, score_max = 0.6, 0.9
     env = Env(env_name)
-    targets = deque(maxlen = 100)
-    for i in range(5):
-        x = np.random.rand() * radius
-        y = np.sqrt(radius * radius - x * x)
-        targets.append((x, y))
+    r_min, r_max = 0.1, 0.5
+    replay_buffer = deque(maxlen = 50000)
+    for _ in range(100):
+        r = np.random.rand() * (r_max - r_min) + r_min
+        gx = np.random.rand() * r
+        gy = np.sqrt(r * r - gx * gx)
+        replay_buffer.append((gx, gy))
     for it in range(10000):
-        update_policy(env, targets, 10)
-        mean_length, mean_reward = eval_policy(env, targets)
+        new_goals = []
+        for _ in range(66):
+            r = np.random.rand() * (r_max - r_min) + r_min
+            gx = np.random.rand() * r
+            gy = np.sqrt(r * r - gx * gx)
+            new_goals.append((gx, gy))
+        old_goals = []
+        for _ in range(34):
+            old_goals.append(replay_buffer[np.random.randint(len(replay_buffer))])
+        goals = new_goals + old_goals
+        update_policy(env, goals, 5)
+        results = eval_policy(env, goals)
+        new_r_min, new_r_max = 1E10, 0
+        for score, (gx, gy) in results:
+            if score >= score_min and score <= score_max:
+                new_r_min = min(new_r_min, np.sqrt(gx * gx + gy * gy))
+                new_r_max = max(new_r_max, np.sqrt(gx * gx + gy * gy))
+        if new_r_min > new_r_max:
+            print("WARNING: new_r_min > new_r_max")
+        else:
+            r_min, r_max = new_r_min, new_r_max * 1.1
+        for gx, gy in new_goals:
+            novel = True
+            for old_gx, old_gy in replay_buffer:
+                dx = old_gx - gx
+                dy = old_gy - gy
+                if np.sqrt(dx * dx + dy * dy) <= 0.5:
+                    novel = False
+                    break
+            if novel:
+                replay_buffer.append((gx, gy))
         logger.log("********** Iteration %i ************" % (it))
-        logger.record_tabular("Distance", radius)
-        logger.record_tabular("Mean length", mean_length)
-        logger.record_tabular("Mean reward", mean_reward)
+        logger.record_tabular("Rmin", r_min)
+        logger.record_tabular("Rmax", r_max)
         logger.dump_tabular()
-        if mean_reward < 0.6:
-            radius *= 0.8
-        elif mean_reward > 0.99:
-            radius *= 1.2
-            for i in range(5):
-                x = np.random.rand() * radius
-                y = np.sqrt(radius * radius - x * x)
-                targets.append((x, y))
-
-    '''
-    stats_losses= []
-    for raw_data in data_gen:
-        stats_length.extend(raw_data.pop("ep_length"))
-        stats_reward.extend(raw_data.pop("ep_reward"))
-        for batch in data:
-            feed_dict = {pi.inputs: batch["s"], pi_old.inputs: batch["s"], a_taken: batch["a"], adv_sample: batch["adv"], v_sample: batch["g"]}
-            stats_losses.append(sess.run(losses, feed_dict))
-    summary_feed = dict()
-    mean_losses = np.mean(np.array(stats_losses), axis = 0)
-    for name, loss in zip(losses_names, mean_losses):
-        logger.record_tabular("loss_" + name, loss)
-        summary_feed[summary_losses[name]] = loss
-    mean_length = np.mean(np.array(stats_length), axis = 0)
-    summary_feed[summary_length] = mean_length
-    mean_reward = np.mean(np.array(stats_reward), axis = 0)
-    summary_feed[summary_reward] = mean_reward
-    summary_feed[step] = it * horizon
-    summary_str = sess.run(summary_op, feed_dict = summary_feed)
-    summary_writer.add_summary(summary_str, it * horizon)
-    saver.save(sess, logdir + "/ppo", it * horizon)
-    '''
